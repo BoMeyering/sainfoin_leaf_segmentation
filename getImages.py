0b0df6896a32c35c2e327041f491d47b80c0eb0e
jzer <1234567890spamemail@gmail.com>
2024-05-28 08:53:11 -0500

    Merged makeImageJson and getImages into a single file.

--- getImages.py
+++ getImages.py
@@ -8,7 +8,7 @@ import requests
 
 
 # Callback used for JSON Converter
-def json_stream_handler(output: lb.JsonConverterOutput, imagePath = 'data/raw/segmentedImages/', jsonPath = 'data/raw/exportProject.ndjson',silent = False):
+def json_stream_handler(output: lb.JsonConverterOutput, jsonFile, imagePath = 'data/raw/segmentedImages/', silent = False):
   #convert the json to a dictionary
   line = json.loads(output.json_str)
   #check for an error where composite_mask does not appear in the json
@@ -29,11 +29,10 @@ def json_stream_handler(output: lb.JsonConverterOutput, imagePath = 'data/raw/se
     file.write(webData)
     file.close()
     #write the output to an ndjson file as well
-    jsonFile = open(jsonPath,mode='a')
     jsonFile.writelines(output.json_str+'\n')
-    jsonFile.close()
 
-
+#retrieves images from label box given a project ID and stores them at imageFolderPath. A ndjson file will be made at jsonPath.
+#silent prevents printing to the console
 def GetImages(projectID,imageFolderPath,jsonPath, silent = False):
   #Connect to the API using the API key
   API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGhpNmJiY2swMGt5MDd3YzlncnA1Z2g4Iiwib3JnYW5pemF0aW9uSWQiOiJjbGhpNmJiYzMwMGt4MDd3Y2Q0aG84cnhoIiwiYXBpS2V5SWQiOiJjbHdqbHFja3YwMDZvMDd4YWVtYWFmb2IyIiwic2VjcmV0IjoiNjNkYWQwNGViYzg2NDViOGYzYTY4ZjMwYTNjNTI4MTQiLCJpYXQiOjE3MTY0ODk4MzksImV4cCI6MjM0NzY0MTgzOX0.DGGptdFGhM8VWn7QyGRQC_JiQvb91KfCPP08zroX9GA"
@@ -74,10 +73,13 @@ def GetImages(projectID,imageFolderPath,jsonPath, silent = False):
     ).start(stream_handler=lambda error: print(error))
   #download images if export suceeds
   if export_task.has_result():
-    export_json = export_task.get_stream(
-      converter=lb.JsonConverter(),
-      stream_type=lb.StreamType.RESULT
-    ).start(stream_handler=json_stream_handler)
+    jsonFile = open(jsonPath, mode='w')
+
+    stream = export_task.get_stream()
+    for output in stream:
+      output: lb.JsonConverterOutput = output
+      json_stream_handler(output,jsonFile=jsonFile,imagePath=imageFolderPath,silent=silent)
+    jsonFile.close()
 
 
   #print the number of lines and file size of the exported json
@@ -87,34 +89,46 @@ def GetImages(projectID,imageFolderPath,jsonPath, silent = False):
 
 
 def MakeImageJson(inputJsonPath, outputJsonPath,silent = False):
-  jsonFile = open('data/raw/exportProject.ndjson',mode='r')
+  #open the file to write the json to
+  jsonFile = open(inputJsonPath,mode='r')
 
   buildDictionary = {}
   lines = 0
-
+#assemble a dictionary in the structure
+# ImageID:{
+#   Index:{
+#     RGB Value, Class
+#   }
+# }
+#
   for line in jsonFile:
-      lines += 1
+    lines += 1
 
-      di = json.loads(line)
+    di = json.loads(line)
 
-      rgbDictionary = {}
-      objects = di['projects']['clixbl663083u07zxhfgxgfio']['labels'][0]['annotations']['objects']
-      for i in range(1,len(objects)):
-          if 'composite_mask' in objects[i]:
-              rgb = objects[i]['composite_mask']['color_rgb']
-              cl = objects[i]['value']
-              rgbDictionary[i] = {'rgb':rgb,'class':cl}
-              
+    rgbDictionary = {}
+    #Objects contains all the information about the mask and rgb values
+    objects = di['projects']['clixbl663083u07zxhfgxgfio']['labels'][0]['annotations']['objects']
+    for i in range(1,len(objects)):
+      if 'composite_mask' in objects[i]:
+        #extract the rgb value for the mask
+        rgb = objects[i]['composite_mask']['color_rgb']
+        #extract the class
+        cl = objects[i]['value']
+        #make them a key value pair in a dictionary
+        rgbDictionary[i] = {'rgb':rgb,'class':cl}
+      #build the dictionary from the image ID and rgb dictionary          
       buildDictionary[di['data_row']['id']] = rgbDictionary
 
-
-  outFile = open('data/processed/rgbPairs.json',mode='w')
+  #open a file to write to
+  outFile = open(outputJsonPath,mode='w')
+  #writ the full dictionary to the file
   json.dump(buildDictionary,outFile,indent=4)
   outFile.close()
   if not silent:
     print(f'wrote {lines} lines to data/processed/rgbPairs.json')
 
-GetImages(projectID='clixbl663083u07zxhfgxgfio', imageFolderPath='data/raw/segmentedImages/', jsonPath='data/raw/exportProject.ndjson')
+#GetImages(projectID='clixbl663083u07zxhfgxgfio', imageFolderPath='data/raw/segmentedImages/', jsonPath='data/raw/exportProject.ndjson')
 MakeImageJson(inputJsonPath='data/raw/exportProject.ndjson', outputJsonPath='data/processed/rgbPairs.json')
 
 
