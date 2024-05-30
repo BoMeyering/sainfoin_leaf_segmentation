87eabf39128c13801c425e92bd277b18195cfb29
jzer <1234567890spamemail@gmail.com>
2024-05-30 09:27:26 -0500

    optimized extraction of bounding boxes.

--- makeBoundingBoxes.py
+++ makeBoundingBoxes.py
@@ -4,34 +4,35 @@ import json
 import numpy as np
 import tqdm
 
+#creates a csv in the form imageID,index,x1,y1,x2,y2
+#It will save to csvSavePath and requires the rgbjson and rgbimage folder
 def processImages(imageSourceFolder, jsonSourceFile, csvSavePath,silent = True):
+    #load the rgb dictionary
     rgbDictionary = json.load(open(jsonSourceFile,'r'))
-
+    #create the csv file for writing
     csv = open(csvSavePath,'w')
+    #for each image ID
     for id in tqdm.tqdm(rgbDictionary):
+        #import the image corosponding to the ID from the dictionary
         image = cv2.imread(imageSourceFolder + id + '.png')
-        # imgs = cv2.resize(image,(400,400))
-        # cv2.imshow('img',imgs)
-        # cv2.waitKey(0)
+        #for each index AKA annotation
         for index in rgbDictionary[id]:
-            color = rgbDictionary[id][index]['rgb']
-            mask = np.zeros((len(image),len(image[0])))
+            #get the color of the anotation
+            rgb = rgbDictionary[id][index]['rgb']
+            bgr = rgb[::-1]
+            bgr = np.array(bgr,dtype=np.uint8)
+            # create a black and white mask image that contains only the anotation
+            mask = cv2.inRange(image, bgr, bgr)
 
-            brgcolor = (color[2],color[1],color[0])
-
-            mask = (image[:, :, 0:3] == brgcolor).all(2) * 255
-            npmask = np.array(mask,dtype=np.uint8)
-            
-            
-            # imgs = cv2.resize(npmask,(400,400))
-            # cv2.imshow('img',imgs)
-            # cv2.waitKey(0)
-
-            contours, hierarchy = cv2.findContours(npmask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-            if len(contours) > 0:
-                x,y,w,h = cv2.boundingRect(contours[0])
+            # extract the contours form the black and white mask
+            cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # make sure a contour was extracted before accessing it
+            if len(cnts) > 0:
+                #convert x,y,w,h to x1,y1,x2,y2
+                x,y,w,h = cv2.boundingRect(cnts[0])
                 x2 = x+w
-                y2 = y+h 
+                y2 = y+h
+                #write the line to the csv file
                 csv.write(f'{id},{index},{x},{y},{x2},{y2}\n')
                 if not silent:
                     print(f'X:{x} Y:{y} W:{w} h:{h}')
